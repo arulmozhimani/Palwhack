ed2bf69fc0a95b014f05dc1b4b1dc160209176d3
arulmozhimani <arulmozhimanikandan.n@cognizant.com>
2014-08-20 12:43:16 +0530

    Android Phonegap Base Code
    Phonegap Android app

---
+++ android/sdk/gen/com/example/palwhack/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.palwhack;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ android/sdk/src/com/example/palwhack/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.palwhack;
+
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import org.apache.cordova.*;
+
+public class MainActivity extends DroidGap {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        super.loadUrl("file:///android_asset/www/index.html");
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+    }
+
+    
+}
+ 
